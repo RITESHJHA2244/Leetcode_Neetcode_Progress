f4a19a89837915fdb6e7b2153932c0bd1da131ef
RITESH JHA <riteshjha2244dtu@gmail.com>
2026-08-20 23:03:15 +0530

    Time: 19 ms (76.68%), Space: 16.3 MB (41.32%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        d={}
+        for word in strs:
+            key=''.join(sorted(word))
+
+            if key not in d:
+                d[key]=[]
+
+            d[key].append(word)
+        return list(d.values())
